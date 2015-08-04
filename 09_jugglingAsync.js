b1174656d0fe978845430c2ff571969405fa8459
Chase Starr <chasestarr@gmail.com>
2015-08-03 22:52:30 -0400

    Working on lesson 09 Juggling Async

---
+++ 09_jugglingAsync.js
@@ -0,0 +1,16 @@
+var bl = require('bl');
+var http = require('http');
+var fs = require('fs');
+var texts = [];
+
+var sites = [process.argv[2],process.argv[3],process.argv[4]]; 
+
+http.get(sites[1], function finishGet(response){
+	response.pipe(bl(function(err,data, function waitCount()){
+		// if err throw err;
+		var text = data.toString();
+		console.log(text);
+	}))	
+})
+
+
